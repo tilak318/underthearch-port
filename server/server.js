63cc308f7527cf6e4f729dfdeba526aa27e210e7
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-03-26 12:19:37 +0530

    Your commit message here

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ app.use(bodyParser.json());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
