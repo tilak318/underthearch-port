84066c890b97f321bfb57d1279b5ca4a5817229a
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-04-14 15:08:08 +0530

    Your commit message here

--- client/src/components/ui/recognitionData.tsx
+++ client/src/components/ui/recognitionData.tsx
@@ -9,7 +9,7 @@ export interface Article {
 
 export const recognitionsData: Article[] = [
   {
-    title: "The White Abode - Archello",
+    title: "Archello - Website",
     url: "https://archello.com/project/the-white-abode",
     source: "Archello",
     date: "2024",
@@ -17,7 +17,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743577041/13-resized_arqcb2.png"
   },
   {
-    title: "The White Abode - Rethinking The Future",
+    title: "Rethinking The Future - Website",
     url: "https://www.re-thinkingthefuture.com/interior-design/10137-the-white-abode-by-under-the-arch/",
     source: "Rethinking The Future",
     date: "2024",
@@ -25,7 +25,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743577041/13-resized_arqcb2.png"
   },
   {
-    title: "The White Abode - Architizer",
+    title: "Architizer - Website",
     url: "https://architizer.com/projects/the-white-abode/",
     source: "Architizer",
     date: "2024",
@@ -33,7 +33,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743577041/13-resized_arqcb2.png"
   },
   {
-    title: "The White Abode - Amazing Architecture",
+    title: "Amazing Architecture - Website",
     url: "https://www.amazingarchitecture.com/tags/the-white-abode",
     source: "Amazing Architecture",
     date: "2024",
@@ -41,7 +41,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743577041/13-resized_arqcb2.png"
   },
   {
-    title: "The Seraphic - Archello",
+    title: "Archello - Website",
     url: "https://archello.com/project/seraphic",
     source: "Archello",
     date: "2024",
@@ -49,7 +49,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743579183/ts4-resized_kbeaky.png"
   },
   {
-    title: "The Seraphic - Architizer",
+    title: "Architizer - Website",
     url: "https://architizer.com/projects/seraphic/",
     source: "Architizer",
     date: "2024",
@@ -57,7 +57,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743579183/ts4-resized_kbeaky.png"
   },
   {
-    title: "The Seraphic - Amazing Architecture",
+    title: "Amazing Architecture - Website",
     url: "https://amazingarchitecture.com/apartments/seraphic-ahmedabad-india-by-under-the-arch",
     source: "Amazing Architecture",
     date: "2024",
@@ -65,7 +65,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743579183/ts4-resized_kbeaky.png"
   },
   {
-    title: "The White Abode - Instagram Post 1",
+    title: "Dream Hom’Z - Instagram",
     url: "https://www.instagram.com/p/C6EgALAJnTq/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     source: "Dream Hom’Z ",
     date: "2024",
@@ -73,7 +73,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743577041/13-resized_arqcb2.png"
   },
   {
-    title: "The White Abode - Instagram Post 2",
+    title: "Architects in India - Instagram",
     url: "https://www.instagram.com/p/C429gDYSB_-/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     source: "Architects in India",
     date: "2024",
@@ -81,7 +81,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743577041/13-resized_arqcb2.png"
   },
   {
-    title: "The White Abode - Instagram Post 3",
+    title: "Design Talks India - Instagram",
     url: "https://www.instagram.com/p/C32n6GSS9Li/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     source: "Design Talks India",
     date: "2024",
@@ -89,7 +89,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743577041/13-resized_arqcb2.png"
   },
   {
-    title: "Rasam Fashion - Architizer",
+    title: "Architizer - Website",
     url: "https://architizer.com/projects/rasam-fashion-boutique-a-fusion-of-tradition-and-modernity/",
     source: "Architizer",
     date: "2024",
@@ -97,7 +97,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743578233/rf10-resized_tziz5s.png"
   },
   {
-    title: "Rasam Fashion - Rethinking The Future",
+    title: "Rethinking The Future - Website",
     url: "https://www.re-thinkingthefuture.com/architecture/retail/11313-rasam-fashion-boutique-a-fusion-of-tradition-and-modernity-by-under-the-arch/",
     source: "Rethinking The Future",
     date: "2024",
@@ -105,7 +105,7 @@ export const recognitionsData: Article[] = [
     projectImage: "https://res.cloudinary.com/daasgedae/image/upload/v1743578233/rf10-resized_tziz5s.png"
   },
   {
-    title: "Rasam Fashion - Archello",
+    title: "Archello - Website",
     url: "https://archello.com/project/rasam-fashion-boutique",
     source: "Archello",
     date: "2024",
