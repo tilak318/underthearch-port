6f27e65f3d7de968e885caba10f93be9d443a25c
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-04-15 17:47:25 +0530

    Your commit message here

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -33,14 +33,14 @@ const Footer = () => {
               </a>
             </div>
             {/* Contact details */}
-            <div className="space-y-2.5">
+            <div className="space-y-4">
               <div className="flex items-center space-x-2">
                 <Phone className="text-gray-400 flex-shrink-0" size={18} />
-                <a href="tel:+918238169574" className="text-gray-400 hover:text-white transition-colors" target="_blank">+91 82381 69574</a>
+                <a href="tel:+918238169574" className="text-gray-400 hover:text-white transition-colors text-[17px]" target="_blank">+91 82381 69574</a>
               </div>
               <div className="flex items-center space-x-2">
                 <Mail className="text-gray-400 flex-shrink-0" size={18} />
-                <a href="mailto:underthearch.in@gmail.com" className="text-gray-400 hover:text-white transition-colors" target="_blank">underthearch.in@gmail.com</a>
+                <a href="mailto:underthearch.in@gmail.com" className="text-gray-400 hover:text-white transition-colors text-[17px]" target="_blank">underthearch.in@gmail.com</a>
               </div>
             </div>
           </div>
