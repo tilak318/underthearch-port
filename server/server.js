9aaf5ee1169faad3ab9b2ef4514057962cf801ea
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-05-17 11:30:52 +0530

    Your commit message here

--- server/server.js
+++ server/server.js
@@ -43,7 +43,9 @@ const Contact = mongoose.model("Contact", contactSchema);
 
 // Nodemailer Setup
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
